c273face683ecbd1252829dba8af5fc69921190b
swetha0 <gaali.swetha@gmail.com>
2020-05-02 10:47:32 -0400

    form validated

--- script.js
+++ script.js
@@ -8,6 +8,22 @@
 // username should not contain special characters
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
+var button = document.getElementById('submit');  //when user clicks submit
+button.addEventListener("click", checkUserName);  //get username
+function checkUserName() {
+
+    var text1 = document.getElementById('usertext').value;
+    if (text1 == "" || text1.length > 15 || text1.length < 8 || /^[a-zA-ZO-9]*$/.test(text1)) {  //testforallconditions
+        document.getElementById("name_error").innerHTML += "<p>Wrong username</p>";
+        return false;
+    }
+    for (let i = 0; i < text1.length; i++) {                                  //testforspaces
+        if (String.indexOf[i] !== -1) {
+            document.getElementById("name_error").innerHTML += "<p>Wrong username</p>";
+            return false;
+        }
+    }
+}
 
 // Progression 2 
 // Check mail
@@ -15,10 +31,24 @@
 // only @ , . , _ [at,dot,underscore] is allowed
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
+button.addEventListener("click", checkEmail)
+function checkEmail() {
+    var text2 = document.getElementById('emailtext').value;
+    // console.log(text2);//
+    if (!text2.includes('@', 1) || /^[\!#$%&*_+|/>,<;{}[]()]*$/.test(text2)) //email id must have @, if not present wrong email , also checking for special char.
+        document.getElementById("email_error").innerHTML += "<p>Wrong email id</p>";
 
+}
 // Progression 3
 // Check password
 // create a function called as checkPassword()
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+button.addEventListener("click", checkPassword)
+
+function checkPassword() {
+    var text3 = document.getElementById('passwordtext').value;
+    if ((/^[0-9a-zA-Z]+$/.test(text3)))      //cheking for alphanumeric condition
+        document.getElementById("password_error").innerHTML += "<p>Wrong password</p>";
+}
